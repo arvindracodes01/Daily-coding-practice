6c6a80188e6bec257dd891f78fb3844f5b5c6663
Arvindra Yadav <thearvindra.nwg@gmail.com>
2026-07-26 12:40:35 +0530

    Add files via upload

---
+++ func4.py
@@ -0,0 +1,9 @@
+"""
+Write a function called ractangle_area that takes length and bradth 
+as parameters and prints the area.
+"""
+def rectangle_area(length, breadth):
+    area = length * breadth
+    print("Area of rectangle:", area)
+
+rectangle_area(10, 5)
